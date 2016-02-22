0e58fed42a25a044f8f5b441c0808aeffc0e0c50
sonority <stephan.kellermayr@gmail.com>
2016-02-22 12:39:12 +0100

    Update v0.0.2

--- Classes/Hooks/PageRenderer.php
+++ Classes/Hooks/PageRenderer.php
@@ -28,157 +28,111 @@ use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 class PageRenderer
 {
 
-    /**
-     * Array of jQuery version numbers shipped with this extension
-     *
-     * @var array
-     */
-    protected $availableLocalJqueryVersions = [
-        1002000,
-        1002001,
-        1002002,
-        1002003,
-        1002004,
-        1002005,
-        1002006,
-        1003000,
-        1003001,
-        1003002,
-        1004000,
-        1004001,
-        1004002,
-        1004003,
-        1004004,
-        1005000,
-        1005001,
-        1005002,
-        1006000,
-        1006001,
-        1006002,
-        1006003,
-        1006004,
-        1007000,
-        1007001,
-        1007002,
-        1008000,
-        1008001,
-        1008002,
-        1008003,
-        1009000,
-        1009001,
-        1010000,
-        1010001,
-        1010002,
-        1011000,
-        1011001,
-        1011002,
-        2000000,
-        2000001,
-        2000002,
-        2000003,
-        2001000,
-        2001001,
-        2001002,
-        2001003,
-        2001004,
-    ];
+	/**
+	 * Array of jQuery version numbers shipped with this extension
+	 *
+	 * @var array
+	 */
+	protected $availableLocalJqueryVersions = [
+        1010000, 1010001, 1010002,
+		1011000, 1011001, 1011002, 1011003,
+        1012000,
+        2000000, 2000001, 2000002, 2000003,
+		2001000, 2001001, 2001003, 2001004,
+        2002000
+	];
 
-    /**
-     * Array of Content-Delivery-Networks (CDN) with placeholders
-     *
-     * @var array
-     */
-    protected $jQueryCdnUrls = [
-        'local' => '',
-        'google' => '//ajax.googleapis.com/ajax/libs/jquery/%1$s/jquery%2$s.js',
-        'msn' => '//ajax.aspnetcdn.com/ajax/jQuery/jquery-%1$s%2$s.js',
-        'jquery' => '//code.jquery.com/jquery-%1$s%2$s.js',
-        'cloudflare' => '//cdnjs.cloudflare.com/ajax/libs/jquery/%1$s/jquery%2$s.js'
-    ];
+	/**
+	 * Array of Content-Delivery-Networks (CDN) with placeholders
+	 *
+	 * @var array
+	 */
+	protected $jQueryCdnUrls = [
+		'local' => '',
+		'google' => '//ajax.googleapis.com/ajax/libs/jquery/%1$s/jquery%2$s.js',
+		'msn' => '//ajax.aspnetcdn.com/ajax/jQuery/jquery-%1$s%2$s.js',
+		'jquery' => '//code.jquery.com/jquery-%1$s%2$s.js',
+		'cloudflare' => '//cdnjs.cloudflare.com/ajax/libs/jquery/%1$s/jquery%2$s.js',
+        'jsdelivr' => '//cdn.jsdelivr.net/jquery/%1$s/jquery-%1$s%2$s.js'
+	];
 
-    /**
-     * Insert javascript-tags for jQuery
-     *
-     * @param array $params
-     * @param \TYPO3\CMS\Core\Page\PageRenderer $pObj
-     * @return void
-     */
-    public function renderPreProcess($params, $pObj)
-    {
-        // Get plugin-configuration
-        $conf = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_libjquery.']['settings.'];
-        // Generate script-tag for jquery if CDN is set
-        if (!empty($conf['cdn']) && array_key_exists($conf['cdn'], $this->jQueryCdnUrls)) {
-            // Set version-number for CDN
-            if (!(int) $conf['version'] || $conf['version'] === 'latest') {
-                $versionCdn = end($this->availableLocalJqueryVersions);
-            } else {
-                $versionCdn = VersionNumberUtility::convertVersionNumberToInteger($conf['version']);
-            }
-            // Set correct version-number for local version
-            if (!in_array($versionCdn, $this->availableLocalJqueryVersions)) {
-                $versionLocal = $this->getNearestVersion($versionCdn);
-            } else {
-                $versionLocal = $versionCdn;
-            }
-            $fallbackTag = '';
-            // Choose minified version if debug is disabled
-            $minPart = (int) $conf['debug'] ? '' : '.min';
-            // Deliver gzipped-version if compression is activated and client supports gzip (compression done with "gzip --best -k -S .gzip")
-            $gzipPart = (int) $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['compressionLevel'] ? '.gzip' : '';
-            // Set path and placeholders for local file
-            $this->jQueryCdnUrls['local'] = $conf['localPath'] . 'jquery-%1$s%2$s.js';
-            // Generate tags for local or CDN (and fallback)
-            if ($conf['cdn'] === 'local') {
-                // Get local version and replace placeholders
-                $file = sprintf(
-                        $this->jQueryCdnUrls['local'], VersionNumberUtility::convertIntegerToVersionNumber($versionLocal), $minPart
-                    ) . $gzipPart;
-                $file = str_replace(PATH_site, '', GeneralUtility::getFileAbsFileName($file));
-            } else {
-                // Get CDN and replace placeholders
-                $file = sprintf(
-                    $this->jQueryCdnUrls[$conf['cdn']], VersionNumberUtility::convertIntegerToVersionNumber($versionCdn), $minPart
-                );
-                // Generate fallback if required
-                if ((int) $conf['localFallback']) {
-                    // Get local fallback version and replace placeholders
-                    $fileFallback = sprintf(
-                            $this->jQueryCdnUrls['local'], VersionNumberUtility::convertIntegerToVersionNumber($versionLocal), $minPart
-                        ) . $gzipPart;
-                    // Get absolute path to the fallback-file
-                    $fileFallback = str_replace(PATH_site, '', GeneralUtility::getFileAbsFileName($fileFallback));
-                    // Wrap it in some javascript code which will enable the fallback
-                    $fallbackTag = '<script>window.jQuery || document.write(\'<script src="' .
-                        htmlspecialchars($fileFallback) .
-                        '" type="text/javascript"><\/script>\')</script>' . LF;
-                }
-            }
-            $pObj->addJsLibrary('lib_jquery', $file, 'text/javascript', false, true, '|' . LF . $fallbackTag . '', true);
-        }
-    }
+	/**
+	 * Insert javascript-tags for jQuery
+	 *
+	 * @param array $params
+	 * @param \TYPO3\CMS\Core\Page\PageRenderer $pObj
+	 * @return void
+	 */
+	public function renderPreProcess($params, $pObj)
+	{
+		// Get plugin-configuration
+		$conf = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_libjquery.']['settings.'];
+		// Generate script-tag for jquery if CDN is set
+		if (!empty($conf['cdn']) && array_key_exists($conf['cdn'], $this->jQueryCdnUrls)) {
+			// Set version-number for CDN
+			if (!(int) $conf['version'] || $conf['version'] === 'latest') {
+				$versionCdn = end($this->availableLocalJqueryVersions);
+			} else {
+				$versionCdn = VersionNumberUtility::convertVersionNumberToInteger($conf['version']);
+			}
+			// Set correct version-number for local version
+			if (!in_array($versionCdn, $this->availableLocalJqueryVersions)) {
+				$versionLocal = $this->getNearestVersion($versionCdn);
+			} else {
+				$versionLocal = $versionCdn;
+			}
+			$fallbackTag = '';
+			// Choose minified version if debug is disabled
+			$minPart = (int) $conf['debug'] ? '' : '.min';
+			// Deliver gzipped-version if compression is activated and client supports gzip (compression done with "gzip --best -k -S .gzip")
+			$gzipPart = (int) $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['compressionLevel'] ? '.gzip' : '';
+			// Set path and placeholders for local file
+			$this->jQueryCdnUrls['local'] = $conf['localPath'] . 'jquery-%1$s%2$s.js';
+			// Generate tags for local or CDN (and fallback)
+			if ($conf['cdn'] === 'local') {
+				// Get local version and replace placeholders
+				$file = sprintf($this->jQueryCdnUrls['local'], VersionNumberUtility::convertIntegerToVersionNumber($versionLocal), $minPart) . $gzipPart;
+				$file = str_replace(PATH_site, '', GeneralUtility::getFileAbsFileName($file));
+			} else {
+				// Get CDN and replace placeholders
+				$file = sprintf($this->jQueryCdnUrls[$conf['cdn']], VersionNumberUtility::convertIntegerToVersionNumber($versionCdn), $minPart);
+				// Generate fallback if required
+				if ((int) $conf['localFallback']) {
+					// Get local fallback version and replace placeholders
+					$fileFallback = sprintf($this->jQueryCdnUrls['local'], VersionNumberUtility::convertIntegerToVersionNumber($versionLocal), $minPart) . $gzipPart;
+					// Get absolute path to the fallback-file
+					$fileFallback = str_replace(PATH_site, '', GeneralUtility::getFileAbsFileName($fileFallback));
+					// Wrap it in some javascript code which will enable the fallback
+					$fallbackTag = '<script>window.jQuery || document.write(\'<script src="' .
+						htmlspecialchars($fileFallback) .
+						'" type="text/javascript"><\/script>\')</script>' . LF;
+				}
+			}
+			$pObj->addJsLibrary('lib_jquery', $file, 'text/javascript', FALSE, TRUE, '|' . LF . $fallbackTag . '', TRUE);
+		}
+	}
 
-    /**
-     * Return nearest available version number
-     *
-     * @param integer $version string representation of the selected version number
-     * @return integer
-     */
-    protected function getNearestVersion($version)
-    {
-        // Get first available version provided by this extension
-        $selectedVersion = reset($this->availableLocalJqueryVersions);
-        foreach ($this->availableLocalJqueryVersions as $v) {
-            if ($v < $version) {
-                $selectedVersion = $v;
-            } else if ($v == $version) {
-                $selectedVersion = $version;
-                break;
-            } else if ($v > $version) {
-                break;
-            }
-        }
-        return $selectedVersion;
-    }
+	/**
+	 * Return nearest available version number
+	 *
+	 * @param integer $version string representation of the selected version number
+	 * @return integer
+	 */
+	protected function getNearestVersion($version)
+	{
+		// Get first available version provided by this extension
+		$selectedVersion = reset($this->availableLocalJqueryVersions);
+		foreach ($this->availableLocalJqueryVersions as $v) {
+			if ($v < $version) {
+				$selectedVersion = $v;
+			} else if ($v == $version) {
+				$selectedVersion = $version;
+				break;
+			} else if ($v > $version) {
+				break;
+			}
+		}
+		return $selectedVersion;
+	}
 
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "lib_jquery".
  *
- * Auto generated 19-08-2014 16:34
+ * Auto generated 22-02-2016 12:31
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -12,24 +12,24 @@
 
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'JS Library: jQuery',
-	'description' => 'Integrates the jQuery-library from CDN with a local fallback if requested library is not available. All major versions of jQuery (including minified and gzipped-versions) are shipped with this extension.',
+	'description' => 'Integrates the jQuery-library from CDN with a local fallback if requested library is not available. All relevant versions of jQuery (including minified and gzipped-versions) are shipped with this extension.',
 	'category' => 'misc',
-	'version' => '0.0.1',
-	'state' => 'alpha',
+	'version' => '0.0.2',
+	'state' => 'beta',
 	'uploadfolder' => false,
 	'createDirs' => '',
 	'clearcacheonload' => true,
 	'author' => 'Stephan Kellermayr',
 	'author_email' => 'stephan.kellermayr@gmail.com',
 	'author_company' => 'sonority.at - MULTIMEDIA ART DESIGN',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '6.2.0-7.6.99',
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
-    ],
+	'constraints' => [
+		'depends' => [
+			'typo3' => '6.2.0-7.6.99',
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
+		]
+	]
 ];
 
