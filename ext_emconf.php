e27776aa8beb72d566da2b414939422743438995
sonority <stephan.kellermayr@gmail.com>
2016-06-13 22:27:43 +0200

    [TASK] Raised version to 1.0.0 (stable)

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,25 +11,25 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
-	'title' => 'JS Library: jQuery',
-	'description' => 'Integrates the jQuery-library from CDN with a local fallback if requested library is not available. All relevant versions of jQuery (including minified and gzipped-versions) are shipped with this extension.',
-	'category' => 'misc',
-	'version' => '0.0.2',
-	'state' => 'beta',
-	'uploadfolder' => false,
-	'createDirs' => '',
-	'clearcacheonload' => true,
-	'author' => 'Stephan Kellermayr',
-	'author_email' => 'stephan.kellermayr@gmail.com',
-	'author_company' => 'sonority.at - MULTIMEDIA ART DESIGN',
-	'constraints' => [
-		'depends' => [
-			'typo3' => '6.2.0-7.6.99',
-		],
-		'conflicts' => [
-		],
-		'suggests' => [
-		]
-	]
+    'title' => 'JS Library: jQuery',
+    'description' => 'Integrates the jQuery-library from CDN with a local fallback if requested library is not available. All relevant versions of jQuery (including minified and gzipped-versions) are shipped with this extension.',
+    'category' => 'misc',
+    'version' => '1.0.0',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'clearcacheonload' => true,
+    'author' => 'Stephan Kellermayr',
+    'author_email' => 'stephan.kellermayr@gmail.com',
+    'author_company' => 'sonority.at - MULTIMEDIA ART DESIGN',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.0-7.6.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ]
+    ]
 ];
 
