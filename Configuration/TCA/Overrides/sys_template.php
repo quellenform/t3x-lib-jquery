23d31d9df395e966cdcbca772945a67934633377
stephankellermayr <stephan.kellermayr@gmail.com>
2019-06-26 13:32:39 +0200

    [TASK] Moved typoscript to 'Configuration/TCA/Overrides/'

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+// Add static typoscript configurations
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'lib_jquery', 'Configuration/TypoScript', 'JSlibs: jQuery'
+);
